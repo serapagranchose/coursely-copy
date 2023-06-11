bc7d7ab8196d4a5887e83687893f18967898db29
serapagranchose <seraphin.perrot@gmail.com>
2023-06-11 13:49:41 +0900

    :sparkles: Adding layout and pages
    Adding basic layout and pages, only the contact and the index is done; the others are made to do a basic path

---
+++ src/app/version-test/contact/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className='grid justify-items-center'>
+      <div className="w-[800px] bg-white my-[60px] p-[40px] border-solid border-[1px] border-black">
+        <p className='text-4xl font-black'>Contact us</p>
+      </div>
+    </div>
+  )
+}
