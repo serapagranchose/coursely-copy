6284f8e4a7db320e6ce7ac8f0713eb40a3b13fc1
serapagranchose <seraphin.perrot@gmail.com>
2023-06-11 18:58:13 +0900

    :lipstick: Update Tailwind attributes
    Putting margin, padding contents and the triangle corner of the cards

--- src/app/version-test/contact/page.tsx
+++ src/app/version-test/contact/page.tsx
@@ -1,27 +1,30 @@
 export default function Home() {
   return (
     <div className='grid justify-items-center'>
-      <div className="grid gap-5 content-between w-[800px] bg-white my-[60px] p-[40px] border-solid border-[1px] border-black">
+      <div className="grid relative gap-5 content-between w-[800px] h-[536px] bg-white my-[60px] mb-[90px] p-[40px] border-solid border-[1px] border-black">
+        <div className="absolute right-0 top-[-20px] w-[50px] overflow-hidden inline-block">
+          <div className="h-[70px] bg-black -rotate-45 transform origin-bottom-right"></div>
+        </div>
         <p className='text-4xl font-black'>Contact Us</p>
         <div>
-          <div className="flex gap-5">
-            <div>
-              <div>Name</div>
-              <input className='w-full h-[45px] mt-[10px] border-solid border-[1px] border-black px-[10px]' placeholder="Marry"></input>
+          <div className="grid grid-flow-col justify-stretch gap-5">
+            <div className='mt-[10px]'>
+              <div className='text-sm ml-[10px] h-[24px]'>Your name</div>
+              <input className='w-full h-[45px] border-solid border-[1px] border-black px-[10px]' placeholder="Marry"></input>
             </div>
-            <div>
-              <div>Email</div>
-              <input className='w-full h-[45px] mt-[10px] border-solid border-[1px] border-black px-[10px]' placeholder="example@gmail.com"></input>
+            <div className='mt-[10px]'>
+              <div className='text-sm ml-[10px] h-[24px]'>Email</div>
+              <input className='w-full h-[45px] border-solid border-[1px] border-black px-[10px]' placeholder="example@gmail.com"></input>
             </div>
           </div>
           <div className="grid">
-            <div>
-              <div>Subject</div>
-              <input className='w-full h-[45px] mt-[20px] border-solid border-[1px] border-black px-[10px]'></input>
+            <div className='mt-[20px]'>
+              <div className='text-sm ml-[10px] h-[24px]'>Subject</div>
+              <input className='w-full h-[45px] border-solid border-[1px] border-black px-[10px]'></input>
             </div>
-            <div>
-              <div>Message</div>
-              <input className='w-full h-[80px] mt-[20px] border-solid border-[1px] border-black p-[10px]'></input>
+            <div className='mt-[20px]'>
+              <div className='text-sm ml-[10px] h-[24px]'>Message</div>
+              <input className='w-full h-[80px] border-solid border-[1px] border-black p-[10px]'></input>
             </div>
             <button className='mt-[40px] hover:bg-[#c1befa] hover:text-black inset-0 bg-black h-[45px] border-solid border-[1px] border-black text-white text-sm'>
               Send
