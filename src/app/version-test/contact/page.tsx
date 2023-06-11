408fadc97c9d172116242a92e596680f17289b24
serapagranchose <seraphin.perrot@gmail.com>
2023-06-12 01:04:18 +0900

    :iphone: Working on responsivity

--- src/app/version-test/contact/page.tsx
+++ src/app/version-test/contact/page.tsx
@@ -1,13 +1,13 @@
 export default function Home() {
   return (
     <div className='grid justify-items-center'>
-      <div className="grid relative gap-5 content-between w-[800px] h-[536px] bg-white my-[60px] mb-[90px] p-[40px] border-solid border-[1px] border-black">
+      <div className="grid relative gap-5 content-between max-w-[800px] h-[536px] bg-white my-[60px] mb-[90px] p-[40px] border-solid border-[1px] border-black">
         <div className="absolute right-0 top-[-20px] w-[50px] overflow-hidden inline-block">
           <div className="h-[70px] bg-black -rotate-45 transform origin-bottom-right"></div>
         </div>
         <p className='text-4xl font-black'>Contact Us</p>
         <div>
-          <div className="grid grid-flow-col justify-stretch gap-5">
+          <div className="grid grid-flow-col grid-col-1 md:grid-col-2 justify-stretch gap-5">
             <div className='mt-[10px]'>
               <div className='text-sm ml-[10px] h-[24px]'>Your name</div>
               <input className='w-full h-[45px] border-solid border-[1px] border-black px-[10px]' placeholder="Marry"></input>
